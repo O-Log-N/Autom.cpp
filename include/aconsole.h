8280d32f456eb42fc89a4ea93d74f99a18622e1a
dl-dl <dl.toronto@outlook.com>
2016-03-23 22:39:48 -0400

    Issue #1: started

--- include/aconsole.h
+++ include/aconsole.h
@@ -1,5 +1,63 @@
 #ifndef ACONSOLE_H
 #define ACONSOLE_H
 
-#endif
+#include <memory>
+#include "../3rdparty/cppformat/cppformat/format.h"
 
+#ifndef ATRACELEVEL
+#define ATRACELEVEL 2
+#endif
+static_assert( ATRACELEVEL >= 0, "ATRACELEVEL >= 0" );
+static_assert( ATRACELEVEL <= 4, "ATRACELEVEL <= 4" );
+
+namespace autom
+{
+	class Console
+	{
+	public:
+
+		void trace0() {}
+		void trace1() {}
+		void trace2() {}
+		void trace3() {}
+		void trace4() {}
+
+		template< typename... ARGS >
+		void info( const char* formatStr, const ARGS& ... args ) { fmt::print( formatStr, args... ); }
+		void notice() {}
+		void warn() {}
+		void error() {}
+		void critical() {}
+		void alert() {}
+
+		void time() {}
+		void timeEnd() {}
+	};
+
+	class ConsoleWrapper
+	{
+		std::unique_ptr< Console > console;
+
+	public:
+		void assign( std::unique_ptr< Console > console_ )
+		{
+			// TODO: AASSERT0( ! console );
+			console = std::move( console_ );
+		}
+		template< typename... ARGS >
+		void log( const char* formatStr, const ARGS& ... args ) { console->info( formatStr, args... ); }
+		static int traceLevel() { return ATRACELEVEL; }
+
+		ConsoleWrapper() { console = NULL; };
+		ConsoleWrapper( const ConsoleWrapper& ) = delete;
+		ConsoleWrapper& operator =( const ConsoleWrapper& ) = delete;
+	};
+}
+
+#define AATRACE3(...)\
+do {\
+	if(console.traceLevel()>=3)\
+		console.log(__VA_ARGS__);\
+	} while(0)
+
+#endif
--- test/main.cpp
+++ test/main.cpp
@@ -1,25 +1,37 @@
 #include <exception>
-#include "../3rdparty/cppformat/cppformat/format.h"
 
 #include "../include/aasert.h"
 #include "../include/aconsole.h"
 
 using namespace std;
+using namespace autom;
+
+ConsoleWrapper console;
+
+static int divide( int a, int b )
+{
+	return a / b;
+}
 
 int main()
 {
-    try {
+	try
+	{
+		console.assign( unique_ptr< Console >( new Console ) );
+		console.log( "Hello, {0}!", "World" );
+		AATRACE3( "TRACE LEVEL IS {0}, line {1}", ATRACELEVEL, divide( 10, 0 ));
+		throw std::exception( "42" );
 
-        return 0;
-    }
-    catch (const std::exception& e) {
-        fmt::print(stderr, "Exception!");
-        fmt::print(stderr, e.what());
-        return 1;
-    }
-    catch (...) {
-        fmt::print(stderr, "Unknown exception!");
-        return 1;
-    }
+		return 0;
+	}
+	catch( const std::exception& e )
+	{
+		console.log( "Exception '{0}'", e.what() );
+		return 1;
+	}
+	catch( ... )
+	{
+		console.log( "Unknown exception!" );
+		return 2;
+	}
 }
-
