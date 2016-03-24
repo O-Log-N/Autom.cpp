8c7c8315c061775c91bd776d72b362d6de9b5760
dl-dl <dl.toronto@outlook.com>
2016-03-24 19:09:43 -0400

    Issue #1: in progerss

--- test/main.cpp
+++ test/main.cpp
@@ -21,7 +21,7 @@ int main()
 		console.assign( new Console );
 		console.log( "Hello, {0}{1}", "World",'!' );
 		AATRACE3( "TRACE LEVEL IS {0}, line {1}", console.traceLevel(), __LINE__ );
-		throw std::exception( "42" );
+		throw std::runtime_error( "42" );
 
 		return 0;
 	}
