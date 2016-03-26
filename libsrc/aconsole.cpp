3c255be2c8b843629ba52dd4701ccb00375ea489
No Bugs' Hare <nobugs@ithare.com>
2016-03-26 08:23:45 +0100

    table-based implementation of formattedWrite()

--- libsrc/aconsole.cpp
+++ libsrc/aconsole.cpp
@@ -16,4 +16,46 @@ Copyright (C) 2016 OLogN Technologies AG
 
 namespace autom {
 ConsoleWrapper console;
+
+//NB: We have to duplicate messages as MSVC doesn't support single-parameter static_assert() :-(
+static_assert(Console::TRACE==0,"Console::TRACE==0");
+static_assert(Console::INFO==1,"Console::INFO==1");
+static_assert(Console::NOTICE==2,"Console::NOTICE==2");
+static_assert(Console::WARN==3,"Console::WARN==3");
+static_assert(Console::ERROR==4,"Console::ERROR==4");
+static_assert(Console::CRITICAL==5,"Console::CRITICAL==5");
+static_assert(Console::ALERT==6,"Console::ALERT==6");
+
+static const char* defaultFmtStrings[] = {
+	//we don't use generic "{}: {}\n" substituting "TRACE" etc.
+	//  to save an indirection and few CPU cycles for free
+	"TRACE: {}\n",
+	"INFO: {}\n",
+	"NOTICE: {}\n",
+	"WARN: {}\n",
+	"ERROR: {}\n",
+	"CRITICAL: {}\n",
+	"ALERT: {}\n"
+};
+
+void DefaultConsole::formattedWrite( WRITELEVEL lvl, const char* s ) override {
+	//AASSERT() is probably way too harsh here
+	if(lvl >= 0 && lvl <= sizeof(defaultFmtStrings)/sizeof(defaultFmtStrings[0]))
+	        fmt::print(lvl >= ERROR ? std::cerr : std::cout, defaultFmtStrings[lvl], s);
+	else {
+		fmt::print(std::cerr, "ERROR: DefaultConsole::formattedWrite(): unknown lvl={}, forced to ERROR:\n", lvl);
+		fmt::print(std::cerr, "ERROR: {}\n", s);
+	}
 }
+
+void FileConsole::formattedWrite( WRITELEVEL lvl, const char* s ) override {
+	//AASSERT() is probably way too harsh here
+	if(lvl >= 0 && lvl <= sizeof(defaultFmtStrings)/sizeof(defaultFmtStrings[0]))
+	        fmt::print(os, defaultFmtStrings[lvl], s);
+	else {
+		fmt::print(os, "ERROR: DefaultConsole::formattedWrite(): unknown lvl={}, forced to ERROR:\n", lvl);
+		fmt::print(os, "ERROR: {}\n", s);
+	}
+}
+
+}//namespace autom
